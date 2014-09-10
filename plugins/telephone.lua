1dddcd238502074c3ca765ec5f2f3ea7420e8e5c
Breno Ramalho Lemes <breno@br-lemes.net>
2014-09-10 19:10:20 -0400

    Plugin para consultar números de telefone. Bem simples, baseado em um arquivo de texto plano.

---
+++ plugins/telephone.lua
@@ -0,0 +1,29 @@
+
+plug.tel = { }
+
+if not plug.current then plug.current = plug.tel end
+
+plug.tel.icon = ico.telephone
+plug.tel.menuitem = iup.item{
+	title  = "Telefones",
+	action = function() fun.plugopen(plug.tel) end
+}
+
+iup.Insert(gui.menu, nil, plug.tel.menuitem)
+
+function plug.tel:reload()
+	fun.list = { }
+	local datafile = io.open("telephone.txt", "r")
+	if not datafile then return end
+	local search = ""
+	if gui.search.value then
+		search = gui.search.value:
+			gsub("^(%d%d%d%d)%-", "%1%%-"):upper()
+	end
+	for line in datafile:lines() do
+		line = line:upper():gsub("\r", "")
+		if line:find("^%d%d%d%d%-%d%d%d%d") and line:find(search) then
+			table.insert(fun.list, { name = line, icon = ico.telephone })
+		end
+	end
+end
